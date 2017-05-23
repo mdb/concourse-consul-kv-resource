529081c2240787b311da75e05a94d6db751f8a70
Mike Ball <mikedball@gmail.com>
2017-05-22 20:16:40 -0400

    NOOP check

--- assets/check.js
+++ assets/check.js
@@ -2,18 +2,8 @@
 
 'use strict';
 
-const Client = require('./client');
 const handlers = require('./handlers');
 
 process.stdin.on('data', stdin => {
-  const data = JSON.parse(stdin);
-  const source = data.source || {};
-  const client = new Client(source);
-
-  client.get(source.key)
-    .then(value => {
-      handlers.success([])
-    }, rejected => {
-      handlers.fail(rejected);
-    });
+  handlers.success([]);
 });
