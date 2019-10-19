f0f80c0d93d69ffef902db3a56f3d795d6799a31
Mike Ball <mikedball@gmail.com>
2019-10-19 19:08:08 -0400

    harden out tests
    This also protects against scenarios where params is absent or
    missing both a `file` and a `value`.

--- assets/lib/out.js
+++ assets/lib/out.js
@@ -3,25 +3,32 @@ const fs = require('fs');
 
 function getValue(params, sourceDir) {
   return new Promise((resolve, reject) => {
+    if (!params || (!params.value && !params.file)) {
+      reject(new Error('Must pass required `file` or `value` params'));
+      return;
+    }
+
     if (params.value && params.file) {
-      reject(new Error('Both `file` and `value` present in params'));
+      reject(new Error('Both `file` and `value` are present in params'));
+      return;
+    }
+
+    if (params.value) {
+      resolve(params.value);
+      return;
     }
 
     if (params.file) {
       fs.readFile(`${sourceDir}/${params.file}`, (err, val) => {
         if (err) {
           reject(err);
-
           return;
         }
 
         resolve(val.toString().replace(/\n$/, ''));
+        return;
       });
-
-      return;
     }
-
-    resolve(params.value);
   });
 }
 
@@ -32,25 +39,32 @@ function outAction(sourceDir) {
       const source = data.source || {};
       const client = new Client(source);
 
-      getValue(data.params, sourceDir).then(value => {
-        client.set(source.key, value).then(() => {
-          resolve({
-            version: {
-              value: value
-            },
-            metadata: [{
-              name: 'timestamp',
-              // timestamp in milliseconds:
-              value: Date.now().toString()
-            }, {
-              name: 'value',
-              value: value
-            }]
-          });
-        }, rejected => {
-          reject(rejected);
+      getValue(data.params, sourceDir)
+        .then(value => {
+          console.log('HERE: ', value);
+          client.set(source.key, value)
+            .then(() => {
+              resolve({
+                version: {
+                  value: value
+                },
+                metadata: [{
+                  name: 'timestamp',
+                  // timestamp in milliseconds:
+                  value: Date.now().toString()
+                }, {
+                  name: 'value',
+                  value: value
+                }]
+              });
+            })
+            .catch(err => {
+              reject(err);
+            });
+        })
+        .catch(err => {
+          reject(err);
         });
-      });
     });
   });
 }
