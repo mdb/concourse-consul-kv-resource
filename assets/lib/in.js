1431898eb77442ece5f151727b56fca8b5bd742a
Mike Ball <mikedball@gmail.com>
2019-10-26 06:18:09 -0400

    disable in file writing - does this help TravisCI?

--- assets/lib/in.js
+++ assets/lib/in.js
@@ -1,38 +1,22 @@
-const fs = require('fs-extra');
 const Client = require('./client');
 
-function inAction(destDir) {
+function inAction() {
   return new Promise((resolve, reject) => {
     process.stdin.on('data', stdin => {
       const data = JSON.parse(stdin);
       const source = data.source || {};
       const client = new Client(source);
-      const file = `${destDir}/${source.key}`;
 
       client.get(source.key)
         .then(value => {
-          fs.ensureFile(file, err => {
-            if (err) {
-              reject(err);
-              return;
-            }
-
-            fs.writeFile(file, value.value, err => {
-              if (err) {
-                reject(err);
-                return;
-              }
-
-              resolve({
-                version: {
-                  value: value.value
-                },
-                metadata: [{
-                  name: 'value',
-                  value: value.value
-                }]
-              });
-            });
+          resolve({
+            version: {
+              value: value.value
+            },
+            metadata: [{
+              name: 'value',
+              value: value.value
+            }]
           });
         })
         .catch(rejected => {
