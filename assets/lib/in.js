2462a1af3b352966230d3d0babcbe8c06fbafbea
Mike Ball <mikedball@gmail.com>
2019-10-26 06:31:40 -0400

    Revert "disable in file writing - does this help TravisCI?"
    This reverts commit 1431898eb77442ece5f151727b56fca8b5bd742a.

--- assets/lib/in.js
+++ assets/lib/in.js
@@ -1,22 +1,38 @@
+const fs = require('fs-extra');
 const Client = require('./client');
 
-function inAction() {
+function inAction(destDir) {
   return new Promise((resolve, reject) => {
     process.stdin.on('data', stdin => {
       const data = JSON.parse(stdin);
       const source = data.source || {};
       const client = new Client(source);
+      const file = `${destDir}/${source.key}`;
 
       client.get(source.key)
         .then(value => {
-          resolve({
-            version: {
-              value: value.value
-            },
-            metadata: [{
-              name: 'value',
-              value: value.value
-            }]
+          fs.ensureFile(file, err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            fs.writeFile(file, value.value, err => {
+              if (err) {
+                reject(err);
+                return;
+              }
+
+              resolve({
+                version: {
+                  value: value.value
+                },
+                metadata: [{
+                  name: 'value',
+                  value: value.value
+                }]
+              });
+            });
           });
         })
         .catch(rejected => {
